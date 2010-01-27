04e670528d3166f3432c42fe41b9d4f33feb1e25
Hardy Ferentschik <hibernate@ferentschik.de>
2010-01-27 15:11:11 +0000

    METAGEN-18 - Support @ElementCollection.targetClass + its XMl equivalent

---
+++ src/test/java/org/hibernate/jpamodelgen/test/xmlmapped/Boy.java
@@ -0,0 +1,61 @@
+// $Id:$
+/*
+* JBoss, Home of Professional Open Source
+* Copyright 2009, Red Hat, Inc. and/or its affiliates, and individual contributors
+* by the @authors tag. See the copyright.txt in the distribution for a
+* full listing of individual contributors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.hibernate.jpamodelgen.test.xmlmapped;
+
+import java.util.List;
+
+/**
+ * @author Hardy Ferentschik
+ */
+public class Boy {
+	private long id;
+
+	private String name;
+
+	/*
+	 * The mapping in boy.xml specifies as target-class for the element collection java.lang.Integer. This makes no
+	 * sense from a mapping point, but makes it easy to test.
+	 */
+	private List<String> nickNames;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<String> getNickNames() {
+		return nickNames;
+	}
+
+	public void setNickNames(List<String> nickNames) {
+		this.nickNames = nickNames;
+	}
+}
+
+
